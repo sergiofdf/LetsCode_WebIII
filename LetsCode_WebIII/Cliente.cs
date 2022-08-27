2d8659fa5a3ed0fac86cf10caac9834c1c6f6abb
Sérgio Fleury Dias Filho <sergiofdf@gmail.com>
2022-08-26 22:25:53 -0300

    Add project files.

---
+++ LetsCode_WebIII/Cliente.cs
@@ -0,0 +1,28 @@
+﻿namespace LetsCode_WebIII
+{
+    public class Cliente
+    {
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public int Idade { get; set; }
+
+        public Cliente(string cpf, string nome, DateTime dataNascimento)
+        {
+            Cpf = cpf;
+            Nome = nome;
+            DataNascimento = dataNascimento;
+            Idade = ObterIdade();
+        }
+
+        public int ObterIdade()
+        {
+            int idade = DateTime.Now.Year - DataNascimento.Year;
+            if (DateTime.Now.DayOfYear < DataNascimento.DayOfYear)
+            {
+                idade--;
+            }
+            return idade;
+        }
+    }
+}
---
+++ LetsCode_WebIII/Controllers/ClienteController.cs
@@ -0,0 +1,42 @@
+﻿using LetsCode_WebIII.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LetsCode_WebIII.Controllers
+{
+
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase
+    {
+
+        [HttpGet]
+        public List<Cliente> GetInfoCliente()
+        {
+            return ListaClientes.clienteList;
+        }
+
+        [HttpPost]
+        public Cliente PostCliente(Cliente cliente)
+        {
+            ListaClientes.clienteList.Add(cliente);
+            return cliente;
+        }
+
+        [HttpPut]
+        public Cliente UpdateCliente(Cliente clienteAtualizado)
+        {
+            ListaClientes.clienteList.RemoveAll(c => c.Cpf == clienteAtualizado.Cpf);
+            ListaClientes.clienteList.Add(clienteAtualizado);
+            return clienteAtualizado;
+        }
+
+        [HttpDelete]
+        public List<Cliente> DeleteCliente(string cpf)
+        {
+            ListaClientes.clienteList.RemoveAll(c => c.Cpf == cpf);
+            return ListaClientes.clienteList;
+        }
+
+    }
+}
